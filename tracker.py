da62f0509b0cbde181642143d414a66fb3cf5ca6
Carsmaniac <mickledapickle@me.com>
2021-05-24 11:20:34 +1000

    Generate mod manager descriptions
    Mod manager descriptions can now be made, which are similar to plain
    text descriptions, but don't include links.

--- tracker.py
+++ tracker.py
@@ -61,6 +61,7 @@ class DescVars:
         self.workshop_link = selected_ini["Links"]["steam workshop"]
         self.trucky_link = selected_ini["Links"]["trucky"]
         self.sharemods_link = selected_ini["Links"]["sharemods"]
+        self.forums_link = selected_ini["Links"]["forums"]
 
         self.other_game_dict = {"ats":["ets", "Euro Truck Simulator 2"], "ets":["ats", "American Truck Simulator"]}
         self.other_game_short = self.other_game_dict[game_short][0]
@@ -134,7 +135,7 @@ class TrackerApp:
         self.desc_plain_text.grid(row = 3, column = 0, sticky = "news", padx = 5)
         self.desc_short = ttk.Button(self.panel_description, text = "Short description", command = lambda : self.short_description())
         self.desc_short.grid(row = 4, column = 0, sticky = "news", padx = 5, pady = 5)
-        self.desc_mod_manager = ttk.Button(self.panel_description, text = "Mod manager", command = lambda : self.copy_description())
+        self.desc_mod_manager = ttk.Button(self.panel_description, text = "Mod manager", command = lambda : self.mod_manager_description())
         self.desc_mod_manager.grid(row = 5, column = 0, sticky = "news", padx = 5)
         self.description_copier = ttk.Button(self.panel_description, text = "Copy to clipboard", command = lambda : self.copy_description())
         self.description_copier.grid(row = 6, column = 0, sticky = "news", padx = 5, pady = 5)
@@ -175,7 +176,7 @@ class TrackerApp:
         desc += "[img]{}[/img]\n".format(desc_vars.header_image_link)
         desc += desc_vars.short_description + "\n\n"
         if desc_vars.bus_pack:
-            desc += "[b]This mod requires my [url={}]shared bus resources[/url] to work![/b]\n\n".format(bus_resources_workshop)
+            desc += "[b]This mod requires my [url={}]bus resource pack[/url] to work![/b]\n\n".format(bus_resources_workshop)
         if desc_vars.other_pack:
             desc += "{} pack available [url={}]here[/url].\n\n".format(desc_vars.other_game, desc_vars.other_pack_workshop_link)
         if len(desc_vars.paintjobs) >= 1:
@@ -228,7 +229,7 @@ class TrackerApp:
         desc += "[img]{}[/img]\n".format(desc_vars.header_image_link)
         desc += desc_vars.short_description + "\n\n"
         if desc_vars.bus_pack:
-            desc += "[b]This mod requires my [url={}]shared bus resources[/url] to work![/b]\n\n".format(bus_resources_forums)
+            desc += "[b]This mod requires my [url={}]bus resource pack[/url] to work![/b]\n\n".format(bus_resources_forums)
         if desc_vars.other_pack:
             desc += "{} pack available [url={}]here[/url].\n\n".format(desc_vars.other_game, desc_vars.other_pack_forums_link)
         if len(desc_vars.paintjobs) >= 1:
@@ -279,11 +280,11 @@ class TrackerApp:
     def trucky_description(self, *args):
         desc_vars = DescVars(self.game_short, self.variable_selected_mod.get())
         desc = ""
-        desc += "<div style=\"max-width: 650px\">\n"
+        desc += "<div style=\"max-width: 650px\"> <!-- Cars was here ;) -->\n"
         desc += "    <img src=\"{}\" style=\"padding-bottom: 5px\">\n".format(desc_vars.header_image_link)
         desc += "    <p>{}</p>\n".format(desc_vars.short_description)
         if desc_vars.bus_pack:
-            desc += "    <p style=\"font-weight: 700\">This mod requires my <a style=\"color: white; text-decoration: underline\" href=\"{}\">shared bus resources</a>to work!</p>\n".format(bus_resources_trucky)
+            desc += "    <p style=\"font-weight: 700\">This mod requires my <a style=\"color: white; text-decoration: underline\" href=\"{}\">bus resource pack</a>to work!</p>\n".format(bus_resources_trucky)
         if desc_vars.other_pack:
             desc += "    <p>{} pack available <a style=\"color: white; text-decoration: underline\" href=\"{}\">here</a>.</p>\n".format(desc_vars.other_game, desc_vars.other_pack_trucky_link)
         if len(desc_vars.paintjobs) >= 1:
@@ -328,7 +329,7 @@ class TrackerApp:
                 desc += "        <li><a style=\"color: white; text-decoration: underline\" href=\"{}\">{}</a> - {}</li>\n".format(rel[3], rel[0], rel[1])
             desc += "    </ul>\n"
         desc += "    <p style=\"color: white; font-family: Montserrat, sans-serif; font-size: 24px; font-weight: 700\">Enjoy! :)</p>\n"
-        desc += "</div>"
+        desc += "</div>" + desc_vars.forums_link # For copy-pasting into the forum URL field
         self.description_output.delete("1.0", "end")
         self.description_output.insert("1.0", desc)
 
@@ -337,7 +338,7 @@ class TrackerApp:
         desc = ""
         desc += desc_vars.short_description + "\n\n"
         if desc_vars.bus_pack:
-            desc += ">> This mod REQUIRES my shared bus resources to work! <<\n"
+            desc += ">> This mod requires my bus resource pack to work! <<\n"
             desc += "Download it here: {}\n\n".format(bus_resources_forums)
         if len(desc_vars.paintjobs) >= 1:
             desc += "Paintjobs included:\n"
@@ -371,7 +372,7 @@ class TrackerApp:
         if desc_vars.more_info != "":
             desc += desc_vars.more_info + "\n\n"
         if desc_vars.other_pack:
-            desc += "I've also made a pack for {}: {}".format(desc_vars.other_game, desc_vars.other_pack_sharemods_link)
+            desc += "I've also made a pack for {}: {}".format(desc_vars.other_game, desc_vars.other_pack_forums_link)
         desc += "Please don't reupload my mods to other sites. Thanks, and enjoy! :)"
         self.description_output.delete("1.0", "end")
         self.description_output.insert("1.0", desc)
@@ -380,7 +381,47 @@ class TrackerApp:
         pass
 
     def mod_manager_description(self, *args):
-        pass
+        desc_vars = DescVars(self.game_short, self.variable_selected_mod.get())
+        desc = ""
+        desc += desc_vars.short_description + "\n\n"
+        if desc_vars.bus_pack:
+            desc += "This mod requires my bus resource pack to work!\n\n"
+        if desc_vars.other_pack:
+            desc += "{} pack also available\n\n".format(desc_vars.other_game)
+        if len(desc_vars.paintjobs) >= 1:
+            desc += "Paintjobs included:\n"
+            for pj in desc_vars.paintjobs:
+                desc += "- {}\n".format(pj)
+            desc += "\n"
+        if desc_vars.bus_pack:
+            desc += "Buses supported:\n"
+            for veh in desc_vars.truck_mods:
+                desc += "- {}'s {}\n".format(veh.mod_author, veh.name)
+            desc += "\n"
+        else:
+            if len(desc_vars.trucks) + len(desc_vars.truck_mods) >= 1:
+                desc += "Trucks supported:\n"
+                if len(desc_vars.trucks) >= 1:
+                    for veh in desc_vars.trucks:
+                        desc += "- {}\n".format(veh.name)
+                if len(desc_vars.truck_mods) >= 1:
+                    for veh in desc_vars.truck_mods:
+                        desc += "- {}'s {}\n".format(veh.mod_author, veh.name)
+                desc += "\n"
+            if len(desc_vars.trailers) + len(desc_vars.trailer_mods) >= 1:
+                desc += "Trailers supported:\n"
+                if len(desc_vars.trailers) >= 1:
+                    for veh in desc_vars.trailers:
+                        desc += "- {}\n".format(veh.name)
+                if len(desc_vars.trailer_mods) >= 1:
+                    for veh in desc_vars.trailer_mods:
+                        desc += "- {}'s {}\n".format(veh.mod_author, veh.name)
+                desc += "\n"
+        if desc_vars.more_info != "":
+            desc += desc_vars.more_info + "\n\n"
+        desc += "Please don't reupload my mods. Thanks, and enjoy! :)\n"
+        self.description_output.delete("1.0", "end")
+        self.description_output.insert("1.0", desc)
 
     def copy_description(self, *args):
         pass
